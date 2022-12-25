35fe10bbdf1b8cb06492e159c219f3c02266bcd7
Echoes <rajhphuyal44@gmail.com>
2022-12-25 11:36:49 +0000

    update

---
+++ assets/js/landing.js
@@ -0,0 +1,6 @@
+const textBg = () => {
+	let bigtext = document.querySelector("textBg");
+	bigtext.style.background = "url('https://www.bergerpaints.com/imaginecolours/wp-content/uploads/2018/01/Flag-of-Nepal.png')"
+}
+
+setTimeout(textBg, 3000);
